c28da111dda7555561deacb161643730c6a9124a
Jakob Persons <jakobep24@gmail.com>
2021-01-13 08:46:12 -0500

    test passing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+   console.log('The DOM has loaded')
+   let par = document.querySelector('#text')
+   par.innerHTML = 'This is really cool!'
+})
